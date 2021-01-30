41211aa97ec48bfa114c6ece3b6a9ad6feb5e509
QuanTang <quantanguoa@gmail.com>
2021-01-30 18:04:34 +1030

    my forkify app

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "abb30323-f4fe-4b6d-a80a-7f16c1fd6f7d";
+export const MODAL_CLOSE_SEC = 2.5;
